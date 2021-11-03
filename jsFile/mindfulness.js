7bf4f4a0ea3e7ff88153327e2a8a89f238c63e95
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-11-03 15:36:32 -0400

    done with onMouse

--- jsFile/mindfulness.js
+++ jsFile/mindfulness.js
@@ -5,3 +5,42 @@ changePagetoTherapy.addEventListener("click", therapyPage)
 function therapyPage() {
     window.location.href = "therapyPage.html";
 }
+
+
+var innerDiv = document.querySelector(".innerDiv");
+
+innerDiv.addEventListener("mouseover",small)
+
+ 
+function small(){
+
+    let headText = document.getElementById("head");
+    let descText = document.getElementById("des");
+    let numbText = document .getElementById ("numb")
+
+    headText.style.fontSize = "1.2vw"
+    descText.style.fontSize = ".8vw"
+    numbText.style.fontSize = ".9vw"
+    
+    innerDiv.style.width = "96.5%"
+    innerDiv.style.height = "89.5%"
+    innerDiv.style.cursor = "pointer"
+    innerDiv.style.boxShadow = "0 2px 5px 0 rgba(0, 0, 0, 0.2), 0 4px 10px 0 rgba(0, 0, 0, 0.19)"
+}
+
+function removeSmall(){
+    innerDiv.removeEventListener("mouseover", small);
+}
+
+innerDiv.addEventListener("mouseleave",removeSmall);
+
+
+
+
+// .innerDiv:hover{
+//     width: 96 %;
+//     height: 89 %;
+//     cursor: pointer;
+//     box - shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2), 0 4px 10px 0 rgba(0, 0, 0, 0.19);
+
+// }
