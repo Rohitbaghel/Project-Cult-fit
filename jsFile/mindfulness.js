c72d29153d6c831ce984ddcee4b13c0772c5f2ca
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-11-08 16:19:31 -0500

    afterpopUp

--- jsFile/mindfulness.js
+++ jsFile/mindfulness.js
@@ -9,35 +9,6 @@ function therapyPage() {
 }
 
 
-// var innerDiv = document.getElementsByClassName("innerDiv");
-
-// innerDiv.addEventListener("",small)
-
- 
-// function small(){
-
-//     let headText = document.getElementsByClassName("head");
-//     let descText = document.getElementsByClassName("des");
-//     let numbText = document .getElementsByClassName ("numb")
-
-//     headText.style.fontSize = "1.2vw"
-//     descText.style.fontSize = ".5vw"
-//     numbText.style.fontSize = ".9vw"
-    
-//     innerDiv.style.transition = "all .2s ease-in-out";
-//     innerDiv.style.width = "96.5%"
-//     innerDiv.style.height = "89.5%"
-//     innerDiv.style.cursor = "pointer"
-//     innerDiv.style.boxShadow = "0 2px 5px 0 rgba(0, 0, 0, 0.2), 0 4px 10px 0 rgba(0, 0, 0, 0.19)"
-// }
-
-// function removeSmall(){
-//     innerDiv.removeEventListener("mouseover", small);
-// }
-
-// innerDiv.addEventListener("mouseleave",removeSmall);
-
-
 
 function sid(e){
     // console.log(e.target);
--- jsFile/therapyPage.js
+++ jsFile/therapyPage.js
@@ -102,17 +102,51 @@ var c = 0;
 mindFitBox.addEventListener("click", () => {
     
     let dAB = document.getElementById("imgMf");
-        c -= 180;
+        c += 180;
     dAB.style.transform = `rotate(${c}deg)`;
-    dAB.style.transition = " 0.2s ease-in-out";
-     
+    dAB.style.transition = " 0.2s linear";
+    
+    
 })
+
 let imgMf = document.getElementById("imgMf")
 
 imgMf.addEventListener("click", () => {
 
     let dAB = document.getElementById("imgMf")
-    c -= 180
+    c += 180
     dAB.style.transform = `rotate(${c}deg)`
-     
+    dAB.style.transition = " 0.2s linear";
 })
+
+
+//feedback=>
+let yes = document.querySelectorAll(".pinkC");
+
+yes.forEach((el) => {
+    el.addEventListener("click", event =>{
+        el.style.opacity =  "0.2";
+        el.style.cursor= "not-allowed";
+        alert("Thanks for your feedback")
+    })
+})
+
+
+// popUp=>
+var d = 0;
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    
+    d++;
+    if(d%2==1){
+        popup.style.display = "block"
+        
+    }
+    if (d % 2 == 0) {
+        popup.style.display = "none"
+    }
+    
+    
+}
+
