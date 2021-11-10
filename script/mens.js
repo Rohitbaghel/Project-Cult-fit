cf22fc35b8a9c1bd3608ecbd51b70921e7d67ab9
Rohit Baghel <baghel.rohit99@gmail.com>
2021-11-10 12:05:53 +0530

    mens store

---
+++ script/mens.js
@@ -0,0 +1,301 @@
+let data = [
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/Cmwk8DAvU6x2skuP8E76bCw2',
+        name: 'Graphic Play Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 1049',
+        mainPrice: '₹1499',
+        discount: '30% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/LiUQTrPzcPjTHungTTqWnMfx',
+        name: 'Neon Print Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 899',
+        mainPrice: '₹1499',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/H5Gxpzb93YrAbEeLfPCBbeb8',
+        name: 'Graphic Print Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 899',
+        mainPrice: '₹1499',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/2pmhm9Ue3NHAuDoM87DTbqgo',
+        name: 'Supersoft Side Strip...',
+        brand: 'CULTSPORT',
+        price: '₹ 1199',
+        mainPrice: '₹1999',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/ZQKgzK6q6L48rM9ugoGkJiS3',
+        name: 'Comfort Solid Joggers',
+        brand: 'CULTSPORT',
+        price: '₹ 1139',
+        mainPrice: '₹1899',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/iv2aya8Z756Zi7qyj67rScCE',
+        name: 'Workout Shorts with ...',
+        brand: 'CULTSPORT',
+        price: '₹ 979',
+        mainPrice: '₹1399',
+        discount: '30% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/JZqwMNQBkTfgu7WmJq5refgh',
+        name: 'Workout Shorts with ...',
+        brand: 'CULTSPORT',
+        price: '₹ 979',
+        mainPrice: '₹1399',
+        discount: '30% off'
+
+    },
+
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/QPrzrQKtU6Vj69ancCuzRoMQ',
+        name: 'Solid Workout Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1199',
+        discount: '30% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/P7qGC5Q6c5tdZPgNvaCgaXBd',
+        name: 'Solid Workout Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 719',
+        mainPrice: '₹1199',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/FsCTbJoRZ5H2HpCrRQwEN5CL',
+        name: 'Graphic Print Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1399',
+        discount: '40% off'
+
+    },
+
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/YyDzAcmZuCrBbE7vJruCgpjW',
+        name: 'Graphic Print Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 899',
+        mainPrice: '₹1499',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/GBJGn4TQf2syPCxzUJZcwacR',
+        name: 'Graphic Print Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1399',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/xpCvvdPzZ78S4joB3dbUT6zS',
+        name: 'Graphic Print Shorts',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1399',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/bWEvx2ckTgw8FDv7ub8zPJL1',
+        name: 'Vitals Prime Grey Jo...',
+        brand: 'CULTSPORT',
+        price: '₹ 1149',
+        mainPrice: '₹2299',
+        discount: '50% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/pbtztqFSFZ6Uz2nN2w74kSfN',
+        name: 'Neon Highlight Joggers',
+        brand: 'CULTSPORT',
+        price: '₹ 1259',
+        mainPrice: '₹2099',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/1AfwSR8FyWSTK7soLTfbnrAP',
+        name: 'Supersoft Solid Trac...',
+        brand: 'CULTSPORT',
+        price: '₹ 1199',
+        mainPrice: '₹1999',
+        discount: '40% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/U8xzQqqPXoejTQEnEGmKhgkg',
+        name: 'Supersoft Solid Trac...',
+        brand: 'CULTSPORT',
+        price: '₹ 1399',
+        mainPrice: '₹1999',
+        discount: '30% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/vCe2Zfj9T18HgCqJWFNMn8gR',
+        name: 'Supersoft Yoga Tshirt',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1399',
+        discount: '40% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/saU75zjTZVNP2SHC6jh2Wv3J',
+        name: 'Supersoft Yoga Tshirt',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1399',
+        discount: '40% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/sdbgZMfuXbvsvHLRLgzfRaEN',
+        name: 'Supersoft Solid Joggers',
+        brand: 'CULTSPORT',
+        price: '₹ 1264',
+        mainPrice: '₹2299',
+        discount: '45% off'
+
+    },
+
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/S1D5tTSvVpKUiVdqkQAY72e4',
+        name: '5years of Fit Employ...',
+        brand: 'CULTSPORT',
+        price: '₹ 3,199.00',
+        // mainPrice: '₹1399',
+        // discount: '40% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/kCA2rugUPWKLcJFepU65r3Ty',
+        name: 'Branding Logo Graphi...',
+        brand: 'CULTSPORT',
+        price: '₹ 839',
+        mainPrice: '₹1399',
+        discount: '40% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/pTMhuQFpK22g2dNhxR7PrhKp',
+        name: 'Graphic Print Workou...',
+        brand: 'CULTSPORT',
+        price: '₹ 899',
+        mainPrice: '₹1499',
+        discount: '40% off'
+
+    },
+    {
+        img:'https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,c_fill/dpr_2/cultgear-content/VAp9m951DKHGFom1CTrf99oD',
+        name: 'Signature Print Work...',
+        brand: 'CULTSPORT',
+        price: '₹ 899',
+        mainPrice: '₹1499',
+        discount: '40% off'
+
+    },
+    
+
+]
+let container = document.getElementById("container")
+function showproduct(data) {
+data.forEach((pr)=>{
+  let main_div = document.createElement("div")  
+container.append(main_div)
+main_div.onclick = function() {
+    addtocart(pr)
+}
+let img = document.createElement("img")
+img.src = pr.img
+let brand = document.createElement("p")
+brand.textContent = pr.brand
+let name = document.createElement("p")
+name.textContent = pr.name
+// flex div
+let flex_div= document.createElement("div")
+flex_div.setAttribute("class","flex gap-1")
+let div1= document.createElement("div")
+let price = document.createElement("p")
+price.setAttribute("class","text-base font-normal")
+price.textContent = pr.price
+div1.appendChild(price)
+let div2= document.createElement("div")
+let main_price = document.createElement("p")
+main_price.setAttribute("class","text-xs py-1")
+main_price.textContent = pr.mainPrice
+div2.appendChild(main_price)
+let div3= document.createElement("div")
+let discount = document.createElement("p")
+discount.setAttribute("class","text-red-700 text-xs py-1")
+discount.textContent = pr.discount
+div3.appendChild(discount)
+flex_div.append(div1,div2,div3)
+// flex div
+main_div.append(img,brand,name,flex_div)
+
+})
+}
+if(localStorage.getItem("display_product") == null){
+    localStorage.setItem("display_product",JSON.stringify([]))
+}
+
+if(localStorage.getItem("mens_store") == null){
+    localStorage.setItem("mens_store",JSON.stringify([]))
+}
+
+function addtocart(pr){
+   
+    let display_product =JSON.parse(localStorage.getItem("display_product")) 
+    display_product= []
+    display_product.push(pr)
+    localStorage.setItem("display_product",JSON.stringify(display_product))
+    console.log(display_product)
+
+    let product =JSON.parse(localStorage.getItem("mens_store")) 
+    // product= []
+    product.push(pr)
+    localStorage.setItem("mens_store",JSON.stringify(product))
+    console.log(product)
+
+}
+showproduct(data)
+
+
